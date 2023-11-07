1328769aed591734681bd42fff082a49c2b9c741
Seungkyu Ahn <seungkyua@gmail.com>
2023-11-07 18:29:51 +0900

    Initial commit

---
+++ customer-service/src/test/java/com/example/msapattern/MsaPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msapattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order-service/src/main/java/com/example/msapattern/MsaPatternApplication.java
@@ -0,0 +1,13 @@
+package com.example.msapattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaPatternApplication.class, args);
+	}
+
+}
